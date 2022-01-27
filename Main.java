8af4b88af57c23bf0e9eb7fc4c695f1680c25041
kushal <kushal_vaghani@yahoo.com>
2022-01-27 17:59:06 -0500

    almost final

--- Main.java
+++ Main.java
@@ -3,13 +3,14 @@ import java.util.Scanner;  // Import the Scanner class
 public class Main {
   public static void main(String[] args) {
 
-    Double miValue = 0.0;
-    Double toMiles = 0.62137;
-    int valueInt = 0;
+    Double miValue  = 0.0;
+    Double toMiles  = 0.62137;
+    int valueInt    = 0;
     Double toInches = 0.39;
-    Double toKg = 0.45;
+    Double toKg     = 0.45;
+    Double toF      = 0.0;
 
-    System.out.println("Please enter the number first and after a space unit to convert to e.g. '10 km'");
+    System.out.println("Please enter the number first and after a space unit to convert to \ne.g. '10 km', this converter can convert\n km <-> mi,\n cm <-> in,\n lb <-> kg\n and C <-> F");
 
     Scanner userValue = new Scanner(System.in); // Create a Scanner object
 
@@ -29,7 +30,7 @@ public class Main {
 
 
 
-    if (unitValue.equals("km") || unitValue.equals ("mi") || unitValue.equals ("cm") || unitValue.equals ("in") || unitValue.equals ("lb") || unitValue.equals ("kg"))
+    if (unitValue.equals("km") || unitValue.equals ("mi") || unitValue.equals ("cm") || unitValue.equals ("in") || unitValue.equals ("lb") || unitValue.equals ("kg") || unitValue.equals ("C")|| unitValue.equals ("F"))
     {
       //////converting from km to mi and from mi to km
       if (unitValue.equals ("km")) {
@@ -61,7 +62,19 @@ public class Main {
         miValue = (valueInt / toKg);
         System.out.println(valueEntered + " kg = " + miValue + " lb" );
       }
+
+      else if (unitValue.equals ("C")) {
+        miValue = (valueInt * 1.8) + 32;
+        System.out.println(valueEntered + " C = " + miValue + " F");
+      }
+
+    else if (unitValue.equals ("F")) {
+      miValue = (valueInt - 32) * 0.55555556;
+      System.out.println(valueEntered + " F = " + miValue + " C");
     }
+
+}
+
     else {
       System.out.println("Invalid unit value " + valueEntered + "will not be converted");
     }
